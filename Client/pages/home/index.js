564f93d143bf29f4334c67b1a5f088db79960b34
Duy Nguyen <duynguyen26394@gmail.com>
2020-06-23 21:31:09 +0700

    workbox

--- Client/pages/home/index.js
+++ Client/pages/home/index.js
@@ -31,3 +31,13 @@ ReactDOM.hydrate(
     ,
     document.getElementById('app')
 )
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/dist/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-// require("@babel/polyfill");
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = env => {
 	const isDevBuild = !(env && env.prod);
 	const reStyle = /\.(css|less|styl|scss|sass|sss)$/;
@@ -70,6 +71,13 @@ module.exports = env => {
 					flatten: true,
 				}
 			]),
+			new WorkboxPlugin.GenerateSW({
+				// these options encourage the ServiceWorkers to get in there fast
+				// and not allow any straggling "old" SWs to hang around
+				clientsClaim: true,
+				skipWaiting: true,
+				maximumFileSizeToCacheInBytes: 5000000
+			}),
 		],
 		resolve: {
 			extensions: ['.js', '.jsx'],
