5830b3b8d75b89a4dedfb8a8b883de5127d76168
Duy Nguyen <duynguyen26394@gmail.com>
2020-10-14 12:14:22 +0700

    add traefik proxy

---
+++ deployment.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ $(docker ps -f name=blue -q) ]; then
+  ENV="green"
+  OLD="blue"
+else
+  ENV="blue"
+  OLD="green"
+fi
+
+echo "Pulling latest image"
+docker-compose --pull
+
+echo "Starting "$ENV" container"
+docker-compose --project-name=$ENV up -d
+
+echo "Waiting..."
+sleep 5s
+
+echo "Stopping "$OLD" container"
+docker-compose --project-name=$OLD stop
